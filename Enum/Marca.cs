b56ba97c2d90f52403d8bffa07584665d7f6a21a
juliaSerpa1 <julia_jmsn@hotmail.com>
2021-09-10 01:50:24 -0300

    obj discreto

---
+++ Enum/Marca.cs
@@ -0,0 +1,17 @@
+namespace Cadastro.Officina
+{
+    public enum Marca
+    {
+        Toyota = 1,
+		Volkswagen = 2,
+		Ford = 3,
+		Honda = 4,
+		Hyundai = 5,
+		Nissan = 6,
+		Chevrolet = 7,
+		Kia = 8,
+		Mercedes_Benz = 9,
+		BMW = 10,
+		Tesla = 11
+    }
+}
